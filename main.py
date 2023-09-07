980299a4b2770b71dfb5523d96dcb802b78bf0b1
Timur <113575005+rixittt@users.noreply.github.com>
2023-09-07 11:59:33 +0300

    Update main.py
    Код сначала преобразует строку в нижний регистр с помощью lower(), чтобы сделать проверку регистронезависимой. Затем он сравнивает строку с её обратным вариантом, который создается с помощью среза [::-1]. Если строки совпадают, функция возвращает True, иначе - False.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-a = 112
-print(a)
+def palindrome(input_string):
+    input_string = input_string.lower()
+    return input_string == input_string[::-1]
+
+
+input_str1 = "лепсспел"
+input_str2 = "helloworld"
+
+res1 = palindrome(input_str1)
+res2 = palindrome(input_str2)
+
+print(res1)
+print(res2)
